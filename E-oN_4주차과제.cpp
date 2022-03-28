d7f264475ec4c74c577a913673b8f297e5e41149
gaeun5092 <101084994+gaeun5092@users.noreply.github.com>
2022-03-28 14:40:13 +0900

    Update E-oN_4주차과제.cpp

--- E-oN_4주차과제.cpp
+++ E-oN_4주차과제.cpp
@@ -5,19 +5,20 @@
 main()
 {
 int arr[6];
-int i, j, sw, t;
+int i, j, sw, t, n;
+n=6;
 
 printf("6개의 수를 입력: ");  // 수를 입력받음
-for(i=0; i<6; i++)
+for(i=0; i<n; i++)
     scanf("%d", &arr[i]);
 
-for(i=0; i<5; i++){  //버블정렬
+for(i=0; i<n-1; i++){  //버블정렬
     sw=0;
-    for(j=1; j<5-i; j++)
-        if(arr[j-1] > arr[j]){
-        t=arr[j-1];    
-        arr[j-1]=arr[j];    
-        arr[j]=t;
+    for(j=1; j<n; j++)
+        if( arr[j-1] > arr[j]){
+        t = arr[j-1];    
+        arr[j-1] = arr[j];    
+        arr[j] = t;
         sw=1; 
     }
     if(sw==0)
@@ -26,7 +27,7 @@ for(i=0; i<5; i++){  //버블정렬
 
 printf("[ ");  //출력
 for(i=0;i<6;i++)
-    printf("%d ", arr[i]);
+    printf("%2d", arr[i]);
 printf("]");
 
 }
